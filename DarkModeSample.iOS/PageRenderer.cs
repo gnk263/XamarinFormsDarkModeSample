8e2a983abc88365b2e0d998ee7c5a94ffb848c2a
gnk263 <gnk263@users.noreply.github.com>
2020-05-24 22:58:20 +0900

    add PageRenderer.cs

---
+++ DarkModeSample.iOS/PageRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(DarkModeSample.iOS.PageRenderer))]
+namespace DarkModeSample.iOS
+{
+    public class PageRenderer : Xamarin.Forms.Platform.iOS.PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null) return;
+
+            try
+            {
+                App.ApplyTheme();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
+        {
+            base.TraitCollectionDidChange(previousTraitCollection);
+
+            App.ApplyTheme();
+        }
+    }
+}
