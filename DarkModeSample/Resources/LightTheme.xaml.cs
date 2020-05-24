f88303ca3d6588c5db2573fc364cae9949d4196a
gnk263 <gnk263@users.noreply.github.com>
2020-05-24 21:15:54 +0900

    add Theme file

---
+++ DarkModeSample/Resources/LightTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DarkModeSample.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LightTheme : ResourceDictionary
+    {
+        public LightTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
